224bc37c2e50c7f5af160be2df830188f35748c7
Joaquín Reyes <joaq.reyesm@gmail.com>
2025-03-17 09:31:43 -0300

    commit 21

--- controllers/SurveyController.js
+++ controllers/SurveyController.js
@@ -1,13 +1,31 @@
-import { Survey } from '../models/index.js';
+import { Op } from 'sequelize';
+import db from '../config/db.js'
+import { Survey, Report, Company, SampleSector,SampleSize, Country, Region, Panel } from '../models/index.js';
 
 const createSurvey = async (req, res) => {
+    const t = await db.transaction();
     try {
-        // Extraer datos después de la validación y convertir valores vacíos a null o números
+        // Extraer datos después de la validación
         const {
             Q_S10, Q_S8, Q_S9, Q_S4, Q_S7, Q_A7, Q_A7A, Q_A7B, Q_A11, Q_A7C,
             Q_A7D_address, Q_A7D_estab_name, Q_A9, Q_S12_date, Q_S12_hour,
             Q_S12_inter_name, Q_S12_inter_cargo, companyId
         } = req.body;
+
+        const company = await Company.findByPk(companyId, {
+            include: [
+                { model: SampleSize, attributes: ['code', 'description'] },
+                { model: SampleSector, attributes: ['code', 'description'] },
+                { model: Country, attributes: ['name', 'code'] },
+                { model: Region, attributes: ['name', 'code'] },
+                { model: Panel, attributes:['code', 'description'] },
+            ]
+        });
+
+        if (!company) {
+            return res.status(404).json({ msg: 'Compañía no encontrada' });
+        }
+        console.log(company)
 
         // Convertir valores a números o null si es necesario
         const surveyData = {
@@ -42,16 +60,48 @@ const createSurvey = async (req, res) => {
             (surveyData.Q_A7C === 2 && surveyData.Q_A11 === 1)
         ) {
             surveyData.status = 'Rechazada';
-            await Survey.create(surveyData);
-            return res.status(200).json({ msg: 'Encuesta procesada correctamente' });
         }
+        console.log(company.panel.description,)
+        // Crear encuesta dentro de una transacción
+        const survey = await Survey.create(surveyData, { transaction: t });
+        const reportData = {
+            countryName:company.country.name,
+            countryCode:company.country.code,
+            nameStratificationRegion:company.region.name,
+            regionalStratificationCode:company.region.code,
+            nameSizeStratification:company.sampleSize.description,
+            sizeStratificationCode:company.sampleSize.code,
+            nameStratificationSector:company.sampleSector.description,
+            sectorStratificationCode:company.sampleSector.code,
+            panelName:company.panel.description,
+            panelCode:company.panel.code,
+            eligibilityCode:"N/A",
+            statusCode:"N/A",
+            rejectionCode:"N/A",
+            companyName:company.name,
+            locality:"N/A",
+            address:company.street,
+            zipCode:company.zipCode,
+            contactPerson:"N/A",
+            contactPosition:"N/A",
+            phoneNumberOne:company.phoneNumberOne,
+            phoneNumberSecond:company.phoneNumberSecond || "N/A",
+            faxNumber:company.faxNumber,
+            emailAddress:company.emailAddress,
+            web:"N/A"
+        };
 
-        // Si no fue rechazada, guardar la encuesta con estado 'Confirmada'
-        await Survey.create(surveyData);
+        // Crear reporte dentro de la misma transacción
+        await Report.create(reportData, { transaction: t });
+
+        // Si todo está bien, confirmar la transacción
+        await t.commit();
         res.status(200).json({ msg: 'Encuesta procesada correctamente' });
 
     } catch (error) {
-        console.error(error);
+        // Si hay un error, hacer rollback
+        await t.rollback();
+        console.error('Error:', error);
         res.status(500).json({ msg: 'Error en el servidor' });
     }
 };
